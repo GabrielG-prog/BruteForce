d9f23ccc4a3c29153abdcc9957c0714a5f601444
gg60 <37588805+gg60@users.noreply.github.com>
2020-01-13 09:49:06 +0100

    new

--- bruteforce/index.js
+++ bruteforce/index.js
@@ -1,28 +1,50 @@
-var charset = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+ (function () {
 
-function crack(value) {
-    function toRadix(N, radix) {
-        var HexN = "",
-            Q = Math.floor(Math.abs(N)),
-            R = Q % radix,
-            strv = charset,
-            radix = strv.length;
-        while (true) {
-            HexN = strv.charAt(R) + HexN;
-            Q = (Q - R) / radix;
-            if (Q == 0)
-                break;
-        };
-        return ((N < 0) ? "-" + HexN : HexN);
-    };
-    var start = (new Date()) * 1,
-        cracked = false,
-        index = 0;
-    while (!cracked) {
-        if (toRadix(index) == value)
-            cracked = true;
-        else
-            index++;
-    };
-    alert(((new Date()) * 1) - start);
-};
+     var bf = {
+
+         alphabet: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+         lastRes: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+
+         // allData est un tableau qui stock toutes les possibilités
+         allData: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+
+         run: function (n) { // n est la longueur d'une caine de caractère
+             if (n <= 1) {
+                 console.log("Fin");
+                 return this.allData;
+                 console.log("Longueur = 1 -- fin de la fonction");
+
+                 console.log("Toutes les donées : ", this.allData);
+                 console.log("Dernière valeur :", this.allData[this.allData.length - 1]);
+             } else {
+                 console.log("n-1");
+                 var newRes = [];
+                 for (var i = 0; i < this.lastRes.length; i++) {
+
+                     console.log(i + " = " + this.lastRes[i])
+
+                     //*
+                     for (var j = 0; j < this.alphabet.length; j++) {
+                         //console.log(j + " = " + this.alphabet[j])
+                         var r = this.lastRes[i] + this.alphabet[j];
+                         //console.log(r);
+                         newRes.push(r);
+                         //console.log(this.lastRes);
+                     }
+                     //console.log("Nouvelle réponse = ", newRes);
+
+                 }
+                 console.log("Longueur = " + n + " -- fin de la double boucle ");
+                 this.lastRes = newRes
+                 console.log("Dernière reponse : ", this.lastRes);
+
+                 this.allData = this.allData.concat(this.lastRes)
+                 return this.run(n - 1);
+             }
+
+
+         }
+     };
+     bf.run(3);
+
+ })();
